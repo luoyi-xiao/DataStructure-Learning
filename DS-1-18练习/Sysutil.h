622e8ca83227e4179751e48b6f124979ba2341a7
yixiao-luo <1347968574@qq.com>
2021-01-18 23:34:30 +0800

    顺序表第一次提交

---
+++ DS-1-18练习/Sysutil.h
@@ -0,0 +1,13 @@
+#ifndef _SYSUTIL_H_
+#define _SYSUTIL_H_
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+#include<stdbool.h>
+
+//#include<vld.h>  //内存泄漏工具的头文件
+
+#endif
